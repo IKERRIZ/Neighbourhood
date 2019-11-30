cf722f123b180c4924fd526fecba660cb31f0bb9
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 22:10:56 +0300

    Add  a profile class

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Location(models.Model):
@@ -56,3 +57,50 @@ class Hood(models.Model):
     occupants = models.CharField(max_length=50)
     location = models.ForeignKey(Location)
     created_on = models.DateTimeField(auto_now_add=True, null=True)
+    
+     class Meta:
+        ordering = ['-pk']
+
+    def save_hood(self):
+        self.save()
+
+
+    def delete_hood(self):
+        self.delete()
+
+
+    @classmethod
+    def search_hood(cls, search_term):
+        hood = Hood.objects.filter(name__icontains=search_term)
+        return hood
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile/',blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.CharField(max_length = 255,null = True)
+    full_name = models.CharField(max_length=255, null=True)
+    hood = models.ForeignKey(Hood,null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
