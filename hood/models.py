97d1d0557f547e6743dc46284d51dc394ffb8c5a
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 21:46:46 +0300

    Add class location to the models

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
+
+class Location(models.Model):
+    locations = (
+        ('Nairobi', 'Nairobi'),
+        ('Kiambu', 'Kiambu'),
+        ('Eastlands', 'Eastlands'),
+        ('Machakos', 'Machakos'),
+        ('Nakuru', 'Nakuru'),
+        ('London', 'London'),
+        ('Paris', 'Paris'),
+        ('Vienna', 'Vienna'),
+        ('Sydney', 'Sydney'),
+        ('Stockholm', 'Stockholm'),
+        ('Tokyo', 'Tokyo'),
+        ('Hongkong', 'Hongkong'),        
+        ('Thika', 'Thika'),
+        ('Dubai', 'Dubai'),
+        ('New York', 'New York'),
+        ('Los Angeles', 'Los Angeles'),
+        ('Venice', 'Venice'),
+        ('Cairo', 'Cairo'),
+        ('Himalayas', 'Himalyas'),
+        ('Antarctica', 'Antarctica'),        
+        ('Arctic', 'Arctic'), 
+        ('Fantasy', 'Fantasy'),  
+        ('Sparta', 'Sparta'),
+        ('Mombasa', 'Mombasa'),        
+    )
+    name = models.CharField(max_length=65, choices=locations)
+
+
+
+    def save_loc(self):
+        self.save()
+
+    def delete_loc(self):
+        self.delete()
+
+
+    def __str__(self):
+        return self.name
+
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404
+from .models import *
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
