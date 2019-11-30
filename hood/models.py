0b99795d146488f7b424a2a301e240acf9ef808c
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 22:39:54 +0300

    Import imagefield from pyuploadcare

--- hood/models.py
+++ hood/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from pyuploadcare.dj.models import ImageField
+
+
 # Create your models here.
 
 class Location(models.Model):
