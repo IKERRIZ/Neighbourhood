6933005eb21404d4c6f96a9b567e922311e25100
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 21:47:57 +0300

    Add class category to the models

--- hood/models.py
+++ hood/models.py
@@ -44,3 +44,8 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
