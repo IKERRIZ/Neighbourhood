0e18207c8788622051781c88f335b2666f3dbf48
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 22:54:56 +0300

    Add application configurations

--- hood/urls.py
+++ hood/urls.py
@@ -11,4 +11,12 @@ urlpatterns =[
     url(r'^accounts/update/', views.edit, name='update_profile'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^search_biz/', views.search_category, name='search_category'),
+    url(r'^hood/(?P<hood_id>\d+)', views.hood, name='hood'),
+    url(r'^join(?P<hood_id>\d+)', views.join, name='join'),
+    url(r'^leave/(?P<hood_id>\d+)', views.leave, name='leave'),
+    url(r'^upload_business/', views.upload_business, name='upload_business'),
+    url(r'^post/', views.add_post, name='post'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
