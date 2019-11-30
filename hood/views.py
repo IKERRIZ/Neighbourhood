802c3a35c4c815639fbfe881738c4e9bd86c43d8
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 22:35:30 +0300

    Add function to add new profile

--- hood/views.py
+++ hood/views.py
@@ -48,3 +48,30 @@ def leave(request,hood_id):
     current_user.profile.hood = None
     current_user.profile.save()
     return redirect('home')
+def search_results(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        searched_hood = Hood.search_hood(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search_hood.html',locals())
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
+    
+ # Views for profile
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+
+    profile = User.objects.get(username=username)
+    print(profile.id)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        pass
+    user = request.user
+    profile = User.objects.get(username=username)
+    title = f'@{profile.username} '
+
+    return render(request, 'profile.html', locals())  
