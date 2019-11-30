e676342db3d96d8287ded3c900b4e571f78cb9c9
IKERRIZ <okothfaith94@gmail.com>
2019-11-30 21:20:44 +0300

    Add views function for the login

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    hoods = Hood.objects.all()
+    return render(request,'home.html',locals())
